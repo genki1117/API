13f1184d1d168640d6c8b88bd7526317f86e1d62
豊島　真菜美 <manami-toyoshima@i-enter.co.jp>
2023-01-05 19:33:58 +0900

    必要そうなconst定義の追加

--- app/Domain/Consts/DocumentConst.php
+++ app/Domain/Consts/DocumentConst.php
@@ -7,6 +7,7 @@ namespace App\Domain\Consts;
  */
 class DocumentConst
 {
+    // 書類カテゴリマスタ
     /** @var int */
     const DOCUMENT_CONTRACT = 0;
 
@@ -31,14 +32,14 @@ class DocumentConst
     /** @var string */
     const DOCUMENT_ARCHIVE_NAME = '登録書類';
 
-    const DOCUMENT_TYPE_LIST = [
+    const DOCUMENT_LIST = [
         self::DOCUMENT_CONTRACT => self::DOCUMENT_CONTRACT_NAME,
         self::DOCUMENT_DEAL => self::DOCUMENT_DEAL_NAME,
-        self::DOCUMENT_ARCHIVE => self::DOCUMENT_INTERNAL_NAME,
+        self::DOCUMENT_INTERNAL => self::DOCUMENT_INTERNAL_NAME,
         self::DOCUMENT_ARCHIVE => self::DOCUMENT_ARCHIVE_NAME
     ];
 
-
+    // 書類ステータスマスタ
     /** @var int */
     const DOCUMENT_STATUS_DRAFT = 0;
 
@@ -136,7 +137,7 @@ class DocumentConst
     const DOCUMENT_STATUS_APPROVED_NAME = '承認済';
 
 
-    const DOCUMENT_STATUS = [
+    const DOCUMENT_STATUS_LIST = [
         self::DOCUMENT_STATUS_DRAFT => self::DOCUMENT_STATUS_DRAFT_NAME,
         self::DOCUMENT_STATUS_APPROVALREQUEST => self::DOCUMENT_STATUS_APPROVALREQUEST_NAME,
         self::DOCUMENT_STATUS_SENT => self::DOCUMENT_STATUS_SENT_NAME,
@@ -153,5 +154,91 @@ class DocumentConst
         self::DOCUMENT_STATUS_EXPIRATION => self::DOCUMENT_STATUS_EXPIRATION_NAME,
         self::DOCUMENT_STATUS_SIGNED => self::DOCUMENT_STATUS_SIGNED_NAME,
         self::DOCUMENT_STATUS_APPROVED => self::DOCUMENT_STATUS_APPROVED_NAME
+    ];
+
+    // 保存要件マスタ
+    /** @var int */
+    const DOCUMENT_SCAN_ELECTRONICTRADING = 0;
+
+    /** @var string */
+    const DOCUMENT_SCAN_ELECTRONICTRADING_NAME = '電子取引';
+
+    /** @var int */
+    const DOCUMENT_SCAN_SAVE = 1;
+
+    /** @var string */
+    const DOCUMENT_SCAN_SAVE_NAME = 'スキャン保存';
+
+    /** @var int */
+    const DOCUMENT_SCAN_OTHER = 2;
+
+    /** @var string */
+    const DOCUMENT_SCAN_OTHER_NAME = 'その他';
+
+    const DOCUMENT_SCAN_LIST = [
+        self::DOCUMENT_SCAN_ELECTRONICTRADING => self::DOCUMENT_SCAN_ELECTRONICTRADING_NAME,
+        self::DOCUMENT_SCAN_SAVE => self::DOCUMENT_SCAN_SAVE_NAME,
+        self::DOCUMENT_SCAN_OTHER => self::DOCUMENT_SCAN_OTHER_NAME
+    ];
+
+
+    // 書類種類マスタ
+    /** @var int */
+    const DOCUMENT_TYPE_CONTRACT = 0;
+
+    /** @var string */
+    const DOCUMENT_TYPE_CONTRACT_NAME = '契約書';
+
+    /** @var int */
+    const DOCUMENT_TYPE_QUOTATION = 1;
+
+    /** @var string */
+    const DOCUMENT_TYPE_QUOTATION_NAME = '見積書';
+
+    /** @var int */
+    const DOCUMENT_TYPE_DELIVERYNOTE = 2;
+
+    /** @var string */
+    const DOCUMENT_TYPE_DELIVERYNOTE_NAME = '納品書';
+
+    /** @var int */
+    const DOCUMENT_TYPE_INVOICES = 3;
+
+    /** @var string */
+    const DOCUMENT_TYPE_INVOICES_NAME = '請求書';
+
+    /** @var int */
+    const DOCUMENT_TYPE_RECEIPT = 4;
+
+    /** @var string */
+    const DOCUMENT_TYPE_RECEIPT_NAME = '領収書';
+
+    /** @var int */
+    const DOCUMENT_TYPE_PURCHASEORDERS = 5;
+
+    /** @var string */
+    const DOCUMENT_TYPE_PURCHASEORDERS_NAME = '発注書';
+
+    /** @var int */
+    const DOCUMENT_TYPE_MINUTES = 6;
+
+    /** @var string */
+    const DOCUMENT_TYPE_MINUTES_NAME = '議事録';
+
+    /** @var int */
+    const DOCUMENT_TYPE_OTHER = 7;
+
+    /** @var string */
+    const DOCUMENT_TYPE_OTHER_NAME = 'その他';
+
+    const DOCUMENT_TYPE_LIST = [
+        self::DOCUMENT_TYPE_CONTRACT => self::DOCUMENT_TYPE_CONTRACT_NAME,
+        self::DOCUMENT_TYPE_QUOTATION => self::DOCUMENT_TYPE_QUOTATION_NAME,
+        self::DOCUMENT_TYPE_DELIVERYNOTE => self::DOCUMENT_TYPE_DELIVERYNOTE_NAME,
+        self::DOCUMENT_TYPE_INVOICES => self::DOCUMENT_TYPE_INVOICES_NAME,
+        self::DOCUMENT_TYPE_RECEIPT => self::DOCUMENT_TYPE_RECEIPT_NAME,
+        self::DOCUMENT_TYPE_PURCHASEORDERS => self::DOCUMENT_TYPE_PURCHASEORDERS_NAME,
+        self::DOCUMENT_TYPE_MINUTES => self::DOCUMENT_TYPE_MINUTES_NAME,
+        self::DOCUMENT_TYPE_OTHER => self::DOCUMENT_TYPE_OTHER_NAME
     ];
 }
