6707b8c3d9201dcacde405abbb562c29125884cf
shouta sudo <genki.genki.1117@gmail.com>
2023-01-15 01:08:07 +0900

    ファイルダウンロード作成中

---
+++ app/Http/Controllers/Download/DownloadManagerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Download;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DownloadManagerController extends Controller
+{
+    public function fileDownload (Request $request)
+    {
+        return 'tewst';
+    }
+}
