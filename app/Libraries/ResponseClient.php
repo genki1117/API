d46a27d22ced9406436bbcdf04e5f221e7204b8c
辰巳　浩一 <koichi-tatsumi@i-enter.co.jp>
2023-01-17 19:12:01 +0900

    バリデーションエラーレスポンス形式の修正 ・trait関数に説明文を追加 ・無名関数設定時に、どこで設定した無名関数であるかを判別できるように修正

--- app/Libraries/ResponseClient.php
+++ app/Libraries/ResponseClient.php
@@ -11,7 +11,36 @@ use Illuminate\Contracts\Validation\Validator;
 trait ResponseClient
 {
     /**
-     * 　バリデーション情報をフロント用に加工する
+     * フロント用に加工したバリデーション情報を返却する。
+     *
+     * 【備考】
+     * (１) 本メソッドを使用するクラスは、必ずerrorsTable()関数を実装すること。
+     *
+     * (２) errorsTable()に、無名関数(Closure)を定義している際には、第一引数に該当するfailed情報を渡します。
+     *  (引数のfailed情報 例)
+     *  [
+     *     'key' => 'email',    <---- errorsTable()上のキー名
+     *     'failed' => [        <---- 上記キー名のうち、何要因でエラーとなっているか（配列形式であるが、１要素固定）
+     *         'Max' => [10]          [10]の箇所は、Requestクラス：rules()で、max:10としていれば、10の部分
+     *      ],
+     *     'index' => 1         <---- 上記要因の中のerrorsTable上の何番目で呼ばれたClosureであるか(*1)
+     *  ]
+     *  (*1)errorsTable()に下記が定義されているとすれば、"Max"の中の2つ目(index=1)という意味である。
+     *    'email' => [
+     *           "Required" =>  [
+     *                ["type" => "label", "content" => "user.label.item.email"],
+     *           ],
+     *           "Max" =>  [
+     *               ["type" => "label", "content" => "user.label.item.email"],
+     *               ["type" => "text", "content"  => $callback],
+     *           ],
+     *       ],
+     *
+     * (３) 無名関数(Closure)の設定方法について。
+     *  $callback = function ($failed) {
+     *        return "test";
+     *  };
+     *  上記のように無名関数を作成して、errorsTable上で["type" => "text", "content"  => $callback]のように定義する。
      *
      * @param Validator $validator
      * @return Validator
@@ -53,6 +82,7 @@ trait ResponseClient
             $adjustErrors[$key] = [];
             // 各パラメタ毎のエラーメッセージを１件ずつ取得する。
             foreach ($error as $index => $errorMessage) {
+                // message_keyを設定する。
                 $adjustErrors[$key][$index]['message_key'] = $errorMessage;
                 
                 // $errorsTableから該当するパラメタとエラー要因を検索して、'message'に設定する。
@@ -60,17 +90,25 @@ trait ResponseClient
                     if (preg_match('/'. $tableKey .'/', $key) === 1) {
                         foreach ($errorFactors as $factorKey => $factorValues) {
                             if (array_key_exists($factorKey, $failed[$key])) {
+                                $idx = 0;
                                 foreach ($factorValues as $value) {
-                                    if ($value['content'] instanceof \Closure) {
-                                        // $errorsTableにて、無名関数を設定している場合は、該当するfailed情報を引数として無名関数を呼び出す。
-                                        $failParam = [
-                                            'key' => $key,
-                                            'failed' => [$factorKey => $failed[$key][$factorKey]]
-                                        ];
-                                        $result = $value['content']($failParam);
-                                        $value['content'] = $result;
+                                    if (array_key_exists('content', $value)) {
+                                        // $errorsTableにて、無名関数を設定している場合の処理。
+                                        if ($value['content'] instanceof \Closure) {
+                                            $failParam = [
+                                                'key' => $key,
+                                                'failed' => [$factorKey => $failed[$key][$factorKey]],
+                                                'index' => $idx
+                                            ];
+                                            // 該当するfailed情報を引数として、設定されている無名関数を呼び出す。
+                                            $result = $value['content']($failParam);
+
+                                            // 無名関数の戻り値を'content'に設定する。
+                                            $value['content'] = $result;
+                                        }
                                     }
                                     $adjustErrors[$key][$index]['messages'][] = $value;
+                                    $idx++;
                                 }
                                 // 調整済の[パラメタ][factorKey]を$failedから削除して、以降の検索対象から外す。
                                 unset($failed[$key][$factorKey]);
